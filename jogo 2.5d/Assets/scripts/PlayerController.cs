706e9c3a403b1d599f0d2cc5edef619960803467
JOYCE LIMA SOARES <20211170150069@ifrn.local>
2023-11-23 17:49:07 -0300

    animação

--- jogo 2.5d/Assets/scripts/PlayerController.cs
+++ jogo 2.5d/Assets/scripts/PlayerController.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,6 +18,7 @@ public class PlayerController : MonoBehaviour
 
     private bool isWalk;
     private bool isGround;
+    public LayerMask whatIsGround;
     
     // Start is called before the first frame update
     void Start()
@@ -31,10 +34,25 @@ public class PlayerController : MonoBehaviour
         UpdateAnimator();
     }
 
+    private void FixedUpdate()
+    {
+        isGround = Physics.CheckSphere(transform.position, 0.2f, whatIsGround);
+    }
+
     #region MEUS METODOS
 
     void MoverPersonagem()
     {
+
+        if (newInput.x > 0 && isLookLeft == true)
+        {
+            Flip();
+        }
+        else if (newInput.x < 0 && isLookLeft == false)
+        {
+            Flip();
+        }
+        /*
         if (newInput.x != 0)
         {
             isWalk = true;
@@ -43,23 +61,41 @@ public class PlayerController : MonoBehaviour
         {
             isWalk = false;
         }
+        */
+        isWalk = newInput.x != 0 ? true : false; //IGUAL OS IF ELSE SO QUE MEOR E ORGANIZADO
+        
         
         m_Rigbody.velocity = new Vector3(0f, m_Rigbody.velocity.y, newInput.x * movementSpeed);
     }
 
     void Flip()
     {
-        
+        isLookLeft = !isLookLeft;
+        switch(isLookLeft)
+        {
+            case true:
+                transform.eulerAngles = new Vector3(0f, 180f, 0f);
+                break;
+            
+            case false:
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+                break;
+        }
     }
 
     void UpdateAnimator()
     {
         m_Animator.SetBool("isWalk", isWalk);
+        m_Animator.SetBool("isGround", isGround);
     }
 
     void Pular(bool isPressed)
     {
-        m_Rigbody.AddForce(Vector3.up * jumpForce);
+        if (isPressed = true && isGround == true)
+        {
+             m_Rigbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+       
     }
 
     void Atacar()
